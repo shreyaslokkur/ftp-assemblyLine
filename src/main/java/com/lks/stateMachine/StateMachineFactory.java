99fbf910eeadc3641349e221fd3905561f73b446
Shreyas Lokkur <lokkur@adobe.com>
2015-08-15 17:46:27 +0530

    Removed RJ State

--- src/main/java/com/lks/stateMachine/StateMachineFactory.java
+++ src/main/java/com/lks/stateMachine/StateMachineFactory.java
@@ -29,8 +29,6 @@ public class StateMachineFactory {
             return new NARState();
         else if(RecStatus.RN.equals(recStatus))
             return new RNState();
-        else if(RecStatus.RJ.equals(recStatus))
-            return new RJState();
         else
             return null;
 
