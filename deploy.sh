30b2025307b6060a33e276926e94091910e90f8c
inmanandan <manohar.ra@gmail.com>
2015-08-29 01:00:58 +0530

    Changed sh to use gitbash

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 # Stop tomcat server
-/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/shutdown.bat
+/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/shutdown.bat
 
 # Delete old war and folder
-rm -rf /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/FTPAssemblyLine*
+rm -rf /e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/FTPAssemblyLine*
 
 # Copy new war
-cp target/FTPAssemblyLine.war /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/
+cp target/FTPAssemblyLine.war /e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/
 
 # Start Tomcat
-/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/startup.bat
+/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/startup.bat
