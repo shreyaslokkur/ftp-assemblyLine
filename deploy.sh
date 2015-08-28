b3ec56ea3487e968d726d725fbbe0cda67e454c1
inmanandan <manohar.ra@gmail.com>
2015-08-29 00:58:03 +0530

    Changed path to cygdrive

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 # Stop tomcat server
-/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/shutdown.bat
+/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/shutdown.bat
 
 # Delete old war and folder
-rm -rf /e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/FTPAssemblyLine*
+rm -rf /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/FTPAssemblyLine*
 
 # Copy new war
-cp target/FTPAssemblyLine.war /e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/
+cp target/FTPAssemblyLine.war /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/
 
 # Start Tomcat
-/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/startup.bat
+/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/startup.bat
