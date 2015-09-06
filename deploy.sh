198180e499e3b7d3d56475e27feee9baecc560de
unknown <Administrator@serverdns.vindhyau3.com>
2015-09-06 10:39:47 +0530

    Deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,39 @@
-# Stop tomcat server
-/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/shutdown.bat
+RESULT=`netstat -na | grep '$2' | awk '{print $7}' | wc -l`
+# Stop tomcat server if it is running
+if [ $RESULT != 0 ]; then
+  /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/shutdown.bat
+fi
 
+DIRECTORY=/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/FTPAssemblyLine*
 # Delete old war and folder
-rm -rf /e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/FTPAssemblyLine*
+if [ -d "$DIRECTORY" ]; then
+  # Control will enter here if $DIRECTORY exists.
+  rm -rf "$DIRECTORY"
+fi
+
+DIRECTORY1=/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/ROOT
+if [ -d "$DIRECTORY1" ]; then
+  # Control will enter here if $DIRECTORY exists.
+  rm -rf "$DIRECTORY1"
+fi
 
 # Copy new war
-cp target/FTPAssemblyLine.war /e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/
+cp target/FTPAssemblyLine.war /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/
+
+# Rename war file if it exists
+FILE_STATUS=$([ -f /etc/hosts ] && echo 0 || echo 1 )
+if [ $FILE_STATUS == 0 ]; then
+  mv /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/FTPAssemblyLine.war /cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/webapps/ROOT.war
+fi
 
 # Start Tomcat
-/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/startup.bat
+/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/bin/startup.bat
+
+TOMCAT_HOME=/cygdrive/e/apache-tomcat-7.0.63-windows-x64/apache-tomcat-7.0.63/
+
+wget -O - http://localhost:8080/ >& /dev/null
+if( test $? -eq 0 ) then
+ return 0
+else
+ return 1
+fi
