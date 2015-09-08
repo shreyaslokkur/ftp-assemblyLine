880cbdfacd76dd4d5646c597b099d96eec1df329
ramanohar <manohar.ra@gmail.com>
2015-09-08 22:28:32 +0530

    deleted ROOT

--- startserver.bat
+++ startserver.bat
@@ -4,6 +4,8 @@ netstat -na | find "LISTENING" | find /C /I ":8080" > NUL
 if %errorlevel%==1 (
 	echo "remove FTPAssemblyLine"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine
+	echo "remove ROOT"
+	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\ROOT
 	echo "remove FTPAssemblyLine.war"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine.war
 	echo "copy target\FTPAssemblyLine.war E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\"
@@ -17,6 +19,8 @@ if %errorlevel%==0 (
 	E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\bin\shutdown.bat
 	echo "remove FTPAssemblyLine"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine
+	echo "remove ROOT"
+	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\ROOT
 	echo "remove FTPAssemblyLine.war"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine.war
 	echo "copy target\FTPAssemblyLine.war E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\"
