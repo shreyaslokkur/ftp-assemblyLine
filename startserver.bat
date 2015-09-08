2b851195964947b3efdc384707ba19de27b062c4
ramanohar <manohar.ra@gmail.com>
2015-09-08 22:13:46 +0530

    Updated with echo statements

--- startserver.bat
+++ startserver.bat
@@ -2,15 +2,25 @@
 
 netstat -na | find "LISTENING" | find /C /I ":8080" > NUL
 if %errorlevel%==1 (
+	echo "remove FTPAssemblyLine"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine
+	echo "remove FTPAssemblyLine.war"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine.war
+	echo "copy target\FTPAssemblyLine.war E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\"
 	cp target\FTPAssemblyLine.war E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\
+	echo "startup.bat"
 	E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\bin\startup.bat )
 
 if %errorlevel%==0 (
+
+	echo "shutting doen tomcat"
 	E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\bin\shutdown.bat
+	echo "remove FTPAssemblyLine"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine
+	echo "remove FTPAssemblyLine.war"
 	rm -rf E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\FTPAssemblyLine.war
+	echo "copy target\FTPAssemblyLine.war E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\"
 	cp target\FTPAssemblyLine.war E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\webapps\
 	sleep 10
+	echo "startup.bat"
 	E:\apache-tomcat-7.0.63-windows-x64\apache-tomcat-7.0.63\bin\startup.bat )
