cf6a0cc0a6ef2a62d4d2fb0753d03b28f2d389df
Shreyas Lokkur <lokkur@adobe.com>
2015-10-12 15:05:40 +0530

    Fixed the issue of missing last record

--- src/main/java/com/lks/facade/MainController.java
+++ src/main/java/com/lks/facade/MainController.java
@@ -771,7 +771,7 @@ public class MainController {
 	private int getOffsetFromPageNumber(int pageNumber){
 		int offset;
 		if(pageNumber > 1){
-			offset = (documentUtils.getOffset()*(pageNumber-1))+1;
+			offset = (documentUtils.getOffset()*(pageNumber-1));
 		}else{
 			offset = 0;
 		}
